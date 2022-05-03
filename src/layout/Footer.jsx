c322328f81ad582b90ade8b65607cbce9b2fa242
BINK <bink@email.ua>
2022-05-03 21:54:02 +0300

    finish project

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="page-footer green lighten-4">
+      <div className="footer-copyright">
+        <div className="container">
+          © 2022 Copyright Text
+          <a className="grey-text text-lighten-4 right" href="#!">
+            More Links
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
